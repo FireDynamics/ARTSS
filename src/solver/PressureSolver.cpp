69aa6591f8e825587bf22ffb5b640b05e688d26e
n16h7hunt3r <n16h7hunt3r@nixos>
2020-06-04 23:57:06 +0200

    fixed shadowing / rf PressureSolver

--- src/solver/PressureSolver.cpp
+++ src/solver/PressureSolver.cpp
@@ -1,22 +1,17 @@
-/// \file 		PressureSolver.cpp
-/// \brief 		Defines the steps to solve the pressure Poisson equation
-/// \date 		Sep 13, 2016
-/// \author 	Severt
-/// \copyright 	<2015-2020> Forschungszentrum Juelich GmbH. All rights reserved.
+/// \file       PressureSolver.cpp
+/// \brief      Defines the steps to solve the pressure Poisson equation
+/// \date       Sep 13, 2016
+/// \author     Severt
+/// \copyright  <2015-2020> Forschungszentrum Juelich GmbH. All rights reserved.
 
-#include <iostream>
-#include <spdlog/spdlog.h>
 
 #include "PressureSolver.h"
-#include "../pressure/VCycleMG.h"
-#include "../utility/Parameters.h"
-#include "../Domain.h"
-#include "SolverSelection.h"
-#include "../boundary/BoundaryData.h"
+
 
 PressureSolver::PressureSolver() {
     auto params = Parameters::getInstance();
-    SolverSelection::SetPressureSolver(&this->pres, params->get("solver/pressure/type"), p, rhs);
+    auto p_type = params->get("solver/pressure/type");
+    SolverSelection::SetPressureSolver(&this->pres, p_type, p, rhs);
     control();
 }
 
@@ -24,14 +19,13 @@ PressureSolver::~PressureSolver() {
     delete pres;
 }
 
-//========================================= DoStep =======================================
-// ***************************************************************************************
+//==================================== DoStep ==================================
+// *****************************************************************************
 /// \brief  brings all calculation steps together into one function
-/// \param	dt			time step
-/// \param  sync		synchronization boolean (true=sync (default), false=async)
-// ***************************************************************************************
+/// \param  dt      time step
+/// \param  sync    synchronization boolean (true=sync (default), false=async)
+// *****************************************************************************
 void PressureSolver::DoStep(real t, bool sync) {
-
 #ifndef PROFILING
     spdlog::info("Pressure ...");
 #endif
@@ -39,26 +33,26 @@ void PressureSolver::DoStep(real t, bool sync) {
 // 1. Solve pressure Poisson equation
 
     // local variables and parameters for GPU
-    auto p = SolverI::p;
-    auto rhs = SolverI::rhs;
-    auto d_p = p->data;
-    auto d_rhs = rhs->data;
-
-    size_t bsize = Domain::getInstance()->GetSize(p->GetLevel());
+    auto pi = SolverI::p;
+    auto rhsi = SolverI::rhs;
+    // auto d_p = p->data;
+    // auto d_rhs = rhs->data;
+    // size_t bsize = Domain::getInstance()->GetSize(p->GetLevel());
 
 #pragma acc data present(d_p[:bsize], d_rhs[:bsize])
     {
-        pres->pressure(p, rhs, t, sync);
-    }//end data
+        pres->pressure(pi, rhsi, t, sync);
+    }  // end data
 }
 
-//======================================= Check data ==================================
-// ***************************************************************************************
+//================================== Check data =============================
+// *****************************************************************************
 /// \brief  Checks if field specified correctly
-// ***************************************************************************************
+// *****************************************************************************
 void PressureSolver::control() {
     auto params = Parameters::getInstance();
-    if (params->get("solver/pressure/field") != BoundaryData::getFieldTypeName(FieldType::P)) {
+    auto p_field = params->get("solver/pressure/field");
+    if (p_field != BoundaryData::getFieldTypeName(FieldType::P)) {
         spdlog::error("Fields not specified correctly!");
         std::exit(1);
         //TODO Error handling
