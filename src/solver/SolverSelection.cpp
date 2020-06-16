864ef1affa8fde43d60081ac95677cb80eee0e41
My Linh Wuerzburger <linhwuerzburger@gmx.de>
2020-06-16 17:16:23 +0200

    removed string explicit advect

--- src/solver/SolverSelection.cpp
+++ src/solver/SolverSelection.cpp
@@ -1,8 +1,8 @@
-/// \file 		SolverSelection.cpp
-/// \brief 		Selects the solver
-/// \date 		December 18, 2018
-/// \author 	My Linh Wuerzburger
-/// \copyright 	<2015-2020> Forschungszentrum Juelich GmbH. All rights reserved.
+/// \file       SolverSelection.cpp
+/// \brief      Selects the solver
+/// \date       December 18, 2019
+/// \author     My Linh Wuerzburger
+/// \copyright  <2015-2020> Forschungszentrum Juelich GmbH. All rights reserved.
 
 #include <iostream>
 #include "SolverSelection.h"
@@ -19,7 +19,7 @@
 // =================== Set advection solver ==================
 // ***************************************************************************************
 /// \brief  Sets the advection solver
-/// \param	advectionSolver Pointer to AdvectionSolver
+/// \param  advectionSolver Pointer to AdvectionSolver
 /// \param  advectionType Name of AdvcetionSolver
 // ***************************************************************************************
 void SolverSelection::SetAdvectionSolver(IAdvection **advectionSolver, const std::string& advectionType) {
@@ -36,7 +36,7 @@ void SolverSelection::SetAdvectionSolver(IAdvection **advectionSolver, const std
 // =================== Set diffusion solver ==================
 // ***************************************************************************************
 /// \brief  Sets the diffusion solver
-/// \param	diffusionSolver Pointer to DiffusionSolver
+/// \param  diffusionSolver Pointer to DiffusionSolver
 /// \param  diffusionType Name of DiffusionSolver
 // ***************************************************************************************
 void SolverSelection::SetDiffusionSolver(IDiffusion **diffusionSolver, const std::string& diffusionType) {
@@ -57,7 +57,7 @@ void SolverSelection::SetDiffusionSolver(IDiffusion **diffusionSolver, const std
 // =================== Set pressure solver ==================
 // ***************************************************************************************
 /// \brief  Sets the pressure solver
-/// \param	pressureSolver Pointer to PressureSolver
+/// \param  pressureSolver Pointer to PressureSolver
 /// \param  pressureType Name of PressureSolver
 // ***************************************************************************************
 void SolverSelection::SetPressureSolver(IPressure **pressureSolver, const std::string& pressureType, Field *p, Field *rhs) {
@@ -74,7 +74,7 @@ void SolverSelection::SetPressureSolver(IPressure **pressureSolver, const std::s
 // =================== Set source solver ==================
 // ***************************************************************************************
 /// \brief  Sets the source solver
-/// \param	sourceSolver Pointer to SourceSolver
+/// \param  sourceSolver Pointer to SourceSolver
 /// \param  sourceType Name of SourceSolver
 // ***************************************************************************************
 void SolverSelection::SetSourceSolver(ISource **sourceSolver, const std::string& sourceType) {
@@ -91,7 +91,7 @@ void SolverSelection::SetSourceSolver(ISource **sourceSolver, const std::string&
 // =================== Set turbulence solver ==================
 // ***************************************************************************************
 /// \brief  Sets the turbulence solver
-/// \param	turbulenceSolver Pointer to TurbulenceSolver
+/// \param  turbulenceSolver Pointer to TurbulenceSolver
 /// \param  turbulenceType Name of TurbulenceSolver
 // ***************************************************************************************
 void SolverSelection::SetTurbulenceSolver(ITurbulence **turbulenceSolver, const std::string& turbulenceType) {
