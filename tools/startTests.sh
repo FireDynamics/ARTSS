8f49f88b5874be94610e82195ea5390ba6c2dc25
boehler-max@gmx.de <boehler-max@gmx.de>
2020-10-05 21:55:00 +0200

    adapt to mpi

--- tools/startTests.sh
+++ tools/startTests.sh
@@ -3,31 +3,59 @@ Run all test files in tests folder.
 
 -g|--gpu\trun gpu version
 -m|--multicore\trun multicore version
--s|--serial\trun serial version
+-M|--mpi\trun mpi only version
+-x <integer>\tset number of meshes in x direction
+-y <integer>\tset number of meshes in y direction
+-z <integer>\tset number of meshes in z direction
 "
-COMPILE="serial"
-while [[ $# -gt 0 ]]
-do
-  key="$1"
-  case $key in
-    -h|--help)
+
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--help")       set -- "$@" "-h" ;;
+    "--gpu")        set -- "$@" "-g" ;;
+    "--multicore")  set -- "$@" "-m" ;;
+    "--mpi")        set -- "$@" "-M" ;;
+    *)        set -- "$@" "$arg"
+  esac
+done
+
+X=1
+Y=1
+Z=1
+COMPILE="mpi"
+while getopts ":x:y:z:hgmM" opt; do
+  case $opt in
+    x)
+      X=$OPTARG
+      ;;
+    y)
+      Y=$OPTARG
+      ;;
+    z)
+      Z=$OPTARG
+      ;;
+    h)
       echo -e "$HELP"
-      exit
       ;;
-    -g|--gpu)
+    g)
       COMPILE="gpu"
-      shift
       ;;
-    -m|--multicore)
+    m)
       COMPILE="multicore_cpu"
-      shift
       ;;
-    -s|--serial)
-      COMPILE="serial"
-      shift
+    M)
+      COMPILE="mpi"
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
       ;;
   esac
 done
+
+PROCS=$(($X*$Y*$Z))
+
 cd ..
 p=$(pwd)
 build="$(pwd)/build/bin/artss_$COMPILE"
@@ -38,6 +66,12 @@ for i in ${array[@]}
 do
   echo -e "\n$i"
   cd ${p}/tests/${i}
-  $build Test_*.xml > /dev/null
-  rm *.vtk
+  sed -i -e "s@<MESHX> 1 </MESHX>@<MESHX> $X </MESHX>@" Test_*.xml
+  sed -i -e "s@<MESHY> 1 </MESHY>@<MESHY> $Y </MESHY>@" Test_*.xml
+  sed -i -e "s@<MESHZ> 1 </MESHZ>@<MESHZ> $Z </MESHZ>@" Test_*.xml
+  mpiexec -n $PROCS $build Test_*.xml
+  rm *.vtk *.log
+  sed -i -e "s@<MESHX> $X </MESHX>@<MESHX> 1 </MESHX>@" Test_*.xml
+  sed -i -e "s@<MESHY> $Y </MESHY>@<MESHY> 1 </MESHY>@" Test_*.xml
+  sed -i -e "s@<MESHZ> $Z </MESHZ>@<MESHZ> 1 </MESHZ>@" Test_*.xml
 done
