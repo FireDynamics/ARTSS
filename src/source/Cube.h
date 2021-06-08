6b991f4b2eceffbe937ce5806ff4be18f538a619
My Linh Würzburger <linhwuerzburger@gmx.de>
2021-06-08 11:54:28 +0200

    changed overwrite function according to interface

--- src/source/Cube.h
+++ src/source/Cube.h
@@ -9,17 +9,21 @@
 
 
 #include "../interfaces/ISourceFunction.h"
+#include <random>
 
 class Cube: public ISourceFunction {
 public:
     Cube(real value, real x0, real y0, real z0, real sigma_x, real sigma_y, real sigma_z);
     ~Cube();
     void update_source(Field *out, real t_cur) override;
-    void set_noise(bool has_noise) { m_has_noise = has_noise; }
+    void set_noise(real range, int seed, real step_size) override;
 private:
     Field *m_source_field;
+    int m_has_noise = false;
+    std::mt19937 m_mt;
+    double m_steps;
+    real m_step_size;
     void set_up(real value, real x0, real y0, real z0, real sigma_x, real sigma_y, real sigma_z);
-    bool m_has_noise = false;
 };
 
 
