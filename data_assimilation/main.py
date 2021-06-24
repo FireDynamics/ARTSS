9b60f2d93971b02a33cf1a25fc355ca097e9dc5e
My Linh Würzburger <linhwuerzburger@gmx.de>
2021-06-24 20:11:24 +0200

    added python client

---
+++ data_assimilation/main.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+import time
+
+def config_tcp():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 7777)
+    sock.connect(server_address)
+    return sock
+
+
+def create_message(t_cur, file_name):
+    return str(t_cur) + "|" + file_name
+
+
+def send_message(sock, message):
+    try:
+        # Send data
+        sock.sendall(message)
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len("OK!")
+
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+    finally:
+        print("message was sent successfully")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    socket = config_tcp()
+    time.sleep(10)
+    send_message(create_message(0.1, "test.txt"))
