f9bb9c5e310fb663c1814cad61b3928ca32ff6f2
My Linh Würzburger <linhwuerzburger@gmx.de>
2021-09-01 13:54:05 +0200

    added two tests which control the data pointer after a change in the data field

--- gtests/field/Field.cpp
+++ gtests/field/Field.cpp
@@ -34,6 +34,19 @@ TEST_F(FieldTest, set_val) {
     }
 }
 
+/**
+ * after using the copy function of Field, the pointer to the data array must
+ * still be the same (important for GPU use).
+ */
+TEST_F(FieldTest, copy_data_but_same_pointer) {
+    size_t size = 100;
+    Field a(UNKNOWN_FIELD, 0.0, 0, size);
+    Field b(UNKNOWN_FIELD, 0.5, 0, size);
+    real *data_pointer = a.data;
+    a.copy_data(b);
+    ASSERT_EQ(data_pointer, a.data);
+}
+
 TEST_F(FieldTest, copy_data) {
     size_t size = 100;
     Field a(UNKNOWN_FIELD, 0.0, 0, size);
@@ -66,4 +79,29 @@ TEST_F(FieldTest, swap_field) {
         ASSERT_EQ(b.data[i], x + 0.0);
         x += 1.0;
     }
+}
+
+/**
+ * only content of the fields shall be swapped. Therefore the data pointers have
+ * to remain the same as before.
+ */
+TEST_F(FieldTest, swap_field_but_not_pointers) {
+    size_t size = 100;
+    Field a(UNKNOWN_FIELD, 0.0, 0, size);
+    Field b(UNKNOWN_FIELD, 0.0, 0, size);
+
+    real x = 0.0;
+    for (auto i=0; i < size; ++i) {
+        a.data[i] = x + 0.0;
+        b.data[i] = x + 0.5;
+        x += 1.0;
+    }
+
+    real *data_pointer_a = a.data;
+    real *data_pointer_b = b.data;
+
+    Field::swap(a, b);
+
+    ASSERT_EQ(data_pointer_a, a.data);
+    ASSERT_EQ(data_pointer_b, b.data);
 }
