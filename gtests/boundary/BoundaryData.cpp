f34c6e7f731c10ae032e39a01447094fd9943cde
My Linh Würzburger <linhwuerzburger@gmx.de>
2021-09-03 12:42:30 +0200

    added test for match_patch

--- gtests/boundary/BoundaryData.cpp
+++ gtests/boundary/BoundaryData.cpp
@@ -26,3 +26,20 @@ TEST_F(BoundaryDataTest, match_field) {
     FieldType field_type_unknown = BoundaryData::match_field("a");
     ASSERT_EQ(field_type_unknown, FieldType::UNKNOWN_FIELD);
 }
+
+TEST_F(BoundaryDataTest, match_patch) {
+    Patch patch_front = BoundaryData::match_patch("front");
+    ASSERT_EQ(patch_front, Patch::FRONT);
+    Patch patch_back = BoundaryData::match_patch("back");
+    ASSERT_EQ(patch_back, Patch::BACK);
+    Patch patch_bottom = BoundaryData::match_patch("bottom");
+    ASSERT_EQ(patch_bottom, Patch::BOTTOM);
+    Patch patch_top = BoundaryData::match_patch("top");
+    ASSERT_EQ(patch_top, Patch::TOP);
+    Patch patch_left = BoundaryData::match_patch("left");
+    ASSERT_EQ(patch_left, Patch::LEFT);
+    Patch patch_right = BoundaryData::match_patch("right");
+    ASSERT_EQ(patch_right, Patch::RIGHT);
+    Patch patch_unknown = BoundaryData::match_patch("a");
+    ASSERT_EQ(patch_unknown, Patch::UNKNOWN_PATCH);
+}
