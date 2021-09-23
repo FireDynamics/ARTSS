71046a7da12f15680cad3aeede64b60863317243
My Linh Würzburger <m.wuerzburger@fz-juelich.de>
2021-09-23 13:31:25 +0200

    changed TimeIntegration.cpp

--- src/TimeIntegration.cpp
+++ src/TimeIntegration.cpp
@@ -17,7 +17,7 @@ TimeIntegration::TimeIntegration(SolverController *sc) {
 #ifndef BENCHMARKING
     m_logger = Utility::create_logger(typeid(this).name());
 #endif
-    auto params = Parameters::getInstance();
+    Parameters *params = Parameters::getInstance();
 
     m_dt = params->get_real("physical_parameters/dt");
     m_t_end = params->get_real("physical_parameters/t_end");
@@ -40,40 +40,30 @@ void TimeIntegration::run() {
     Domain *domain = Domain::getInstance();
 
     // local variables and parameters for GPU
-    auto u = m_field_controller->field_u;
-    auto v = m_field_controller->field_v;
-    auto w = m_field_controller->field_w;
-    auto p = m_field_controller->field_p;
-    auto rhs = m_field_controller->field_rhs;
-    auto T = m_field_controller->field_T;
-    auto C = m_field_controller->field_concentration;
-    auto S_T = m_field_controller->field_source_T;
-    auto S_C = m_field_controller->field_source_concentration;
-    auto nu_t = m_field_controller->field_nu_t;
-
-    auto d_u = u->data;
-    auto d_v = v->data;
-    auto d_w = w->data;
-    auto d_p = p->data;
-    auto d_rhs = rhs->data;
-    auto d_T = T->data;
-    auto d_C = C->data;
-    auto d_S_T = S_T->data;
-    auto d_S_C = S_C->data;
-    auto d_nu_t = nu_t->data;
-
-    auto bsize = domain->get_size();
+    Field &u = *m_field_controller->field_u;
+    Field &v = *m_field_controller->field_v;
+    Field &w = *m_field_controller->field_w;
+    Field &p = *m_field_controller->field_p;
+    Field &rhs = *m_field_controller->field_rhs;
+    Field &T = *m_field_controller->field_T;
+    Field &C = *m_field_controller->field_concentration;
+    Field &S_T = *m_field_controller->field_source_T;
+    Field &S_C = *m_field_controller->field_source_concentration;
+    Field &nu_t = *m_field_controller->field_nu_t;
 
 #ifndef BENCHMARKING
-#pragma acc update host(d_u[:bsize])
-#pragma acc update host(d_v[:bsize])
-#pragma acc update host(d_w[:bsize])
-#pragma acc update host(d_p[:bsize])
-#pragma acc update host(d_rhs[:bsize])
-#pragma acc update host(d_T[:bsize])
-#pragma acc update host(d_C[:bsize])
-#pragma acc update host(d_nu_t[:bsize])
-#pragma acc update host(d_S_T[:bsize]) wait    // all in one update does not work!
+    u.update_host();
+    v.update_host();
+    w.update_host();
+    p.update_host();
+    rhs.update_host();
+    T.update_host();
+    C.update_host();
+    nu_t.update_host();
+    S_T.update_host();
+    S_C.update_host();
+#pragma acc wait
+
     m_analysis->analyse(m_field_controller, 0.);
     m_visual->visualise(*m_field_controller, 0.);
     m_logger->info("Start calculating and timing...");
@@ -108,15 +98,17 @@ void TimeIntegration::run() {
             m_solver_controller->solver_do_step(t_cur, false);
 #ifndef BENCHMARKING
             // Visualize
-#pragma acc update host(d_u[:bsize])
-#pragma acc update host(d_v[:bsize])
-#pragma acc update host(d_w[:bsize])
-#pragma acc update host(d_p[:bsize])
-#pragma acc update host(d_rhs[:bsize])
-#pragma acc update host(d_T[:bsize])
-#pragma acc update host(d_C[:bsize])
-#pragma acc update host(d_nu_t[:bsize])
-#pragma acc update host(d_S_T[:bsize]) wait    // all in one update does not work!
+            u.update_host();
+            v.update_host();
+            w.update_host();
+            p.update_host();
+            rhs.update_host();
+            T.update_host();
+            C.update_host();
+            nu_t.update_host();
+            S_T.update_host();
+            S_C.update_host();
+#pragma acc wait
 
             m_solution->calc_analytical_solution(t_cur);
 
@@ -132,7 +124,7 @@ void TimeIntegration::run() {
             m_analysis->calc_L2_norm_mid_point(m_field_controller, t_cur, Sum);
 
             // check CFL
-            real cfl = m_analysis->calc_CFL(u, v, w, dt);
+            real cfl = m_analysis->calc_CFL(&u, &v, &w, dt);
 
             // CFL condition not met
             if (cfl > 1) {
@@ -141,7 +133,7 @@ void TimeIntegration::run() {
             } else {
                 m_logger->info("CFL = {}", cfl);
             }
-            // bool VN_check = ana.check_time_step_VN(u, dt);
+            // bool VN_check = ana.check_time_step_VN(dt);
             // if(!VN_check)
             //     std::cout<<"Von Neumann condition not met!"<<std::endl;
 #endif
@@ -168,16 +160,18 @@ void TimeIntegration::run() {
 #endif
 
 #pragma acc wait
-
-#pragma acc update host(d_u[:bsize])
-#pragma acc update host(d_v[:bsize])
-#pragma acc update host(d_w[:bsize])
-#pragma acc update host(d_p[:bsize])
-#pragma acc update host(d_rhs[:bsize])
-#pragma acc update host(d_T[:bsize])
-#pragma acc update host(d_C[:bsize]) wait
-
-    } // end RANGE
+    u.update_host();
+    v.update_host();
+    w.update_host();
+    p.update_host();
+    rhs.update_host();
+    T.update_host();
+    C.update_host();
+    nu_t.update_host();
+    S_T.update_host();
+    S_C.update_host();
+#pragma acc wait
+    }
 
 #ifndef BENCHMARKING
     m_logger->info("Done calculating and timing ...");
