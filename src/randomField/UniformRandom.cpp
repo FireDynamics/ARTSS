798b376ffa15ba95a4e826f9e6b103078b5a3795
My Linh Würzburger <linhwuerzburger@gmx.de>
2021-11-01 03:23:44 +0100

    added update_dev to uniform random

--- src/randomField/UniformRandom.cpp
+++ src/randomField/UniformRandom.cpp
@@ -8,6 +8,6 @@ Field UniformRandom::random_field(size_t size) {
     for (size_t i=0; i < size; ++i) {
         ret[i] = m_dist(m_mt) * m_step_size;
     }
-
+    ret.update_dev();
     return ret;
 }
