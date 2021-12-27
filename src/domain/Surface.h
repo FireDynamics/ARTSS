5e0f24cf69af12d79a6560f56bc763713a3415b5
My Linh Wuerzburger <linhwuerzburger@gmx.de>
2021-12-28 00:50:46 +0100

    correction: directory name to domain

--- src/domain/Surface.h
+++ src/domain/Surface.h
@@ -4,8 +4,8 @@
 /// \author     My Linh Wuerzburger
 /// \copyright  <2015-2021> Forschungszentrum Juelich GmbH. All rights reserved.
 
-#ifndef ARTSS_BOUNDARY_SURFACE_H_
-#define ARTSS_BOUNDARY_SURFACE_H_
+#ifndef ARTSS_DOMAIN_SURFACE_H_
+#define ARTSS_DOMAIN_SURFACE_H_
 
 #include <vector>
 
@@ -69,4 +69,4 @@ private:
 };
 
 
-#endif /* ARTSS_BOUNDARY_SURFACE_H_ */
+#endif /* ARTSS_DOMAIN_SURFACE_H_ */
