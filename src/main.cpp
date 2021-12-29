d633b4aa068bd67ddd3798a6b546bac04225103f
My Linh Wuerzburger <linhwuerzburger@gmx.de>
2021-12-29 15:34:00 +0100

    added exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -16,33 +16,39 @@
 #endif
 
 int main(int argc, char **argv) {
-    // Initialisation
-    // Parameters
-    std::string XML_filename;
-    if (argc <= 1) {
-        std::cerr << "XML file missing" << std::endl;
-        std::exit(1);
-    }
+    try {
+        // Initialisation
+        // Parameters
+        std::string XML_filename;
+        if (argc <= 1) {
+            std::cerr << "XML file missing" << std::endl;
+            std::exit(1);
+        }
 
 #ifdef _OPENACC
-    // Initialise GPU
-    acc_device_t dev_type = acc_get_device_type();
-    acc_init(dev_type);
+        // Initialise GPU
+        acc_device_t dev_type = acc_get_device_type();
+        acc_init(dev_type);
 #endif
 
-    Settings::Settings_new settings_new = Settings::parse_settings_from_file(argv[1]);
-    Settings::Settings settings(argv[1]);
-    DomainData::init(settings_new);
-    DomainController::init(settings);
+        Settings::Settings_new settings_new = Settings::parse_settings_from_file(argv[1]);
+        Settings::Settings settings(argv[1]);
+        DomainData::init(settings_new);
+        DomainController::init(settings);
 
-    SolverController *sc = new SolverController(settings, settings_new);
+        SolverController *sc = new SolverController(settings, settings_new);
 
-    // Integrate over time and solve numerically
-    // Time integration
-    TimeIntegration ti(settings, sc);
-    ti.run();
+        // Integrate over time and solve numerically
+        // Time integration
+        TimeIntegration ti(settings, sc);
+        ti.run();
 
-    // Clean up
-    delete sc;
+        // Clean up
+        delete sc;
+    } catch (const std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception encountered" << std::endl;
+    }
     return 0;
 }
