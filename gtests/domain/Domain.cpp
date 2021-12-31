13723211f4f0e5aaf25928e98fc15e122cf2a77b
My Linh Wuerzburger <linhwuerzburger@gmx.de>
2021-12-31 16:57:29 +0100

    added index test for computational domain

--- gtests/domain/Domain.cpp
+++ gtests/domain/Domain.cpp
@@ -38,6 +38,8 @@ TEST(DomainTest, sizes) {
     EXPECT_EQ(1004, boundary_sizes.get_sum());
     EXPECT_EQ(1848, domain.get_size_domain_list());
     EXPECT_EQ(1000, domain.get_size_inner_list());
+    delete[] surface_list;
+    delete[] obstacle_list;
 }
 
 TEST(DomainTest, sizesWithObstacle) {
@@ -109,6 +111,10 @@ TEST(DomainTest, sizesWithSurfaces) {
     EXPECT_EQ(1000, boundary_sizes.get_sum());
     EXPECT_EQ(1844, domain.get_size_domain_list());
     EXPECT_EQ(1000, domain.get_size_inner_list());
+
+    for (size_t p = 0; p < number_of_patches; p++) {
+        delete[] surface_list[p];
+    }
     delete[] surface_list;
 }
 
@@ -188,6 +194,54 @@ TEST(DomainTest, order) {
     for (size_t i = 1; i < domain.get_size_domain_list(); i++) {
         EXPECT_GT(domain_list[i], domain_list[i - 1]);
     }
+    delete[] surface_list;
+    delete[] obstacle_list;
+}
+
+TEST(DomainTest, indexTestComputationalDomain) {
+    Settings::domain_parameters domain_params{};
+    domain_params.start_coords_CD.set_coordinate(0, 0, 0);
+    domain_params.start_coords_PD.set_coordinate(0, 0, -1.5);
+    domain_params.end_coords_CD.set_coordinate(5, 10, 15);
+    domain_params.end_coords_PD.set_coordinate(7, 15, 30);
+    domain_params.enable_computational_domain = true;
+    domain_params.number_of_inner_cells.set_coordinate(5, 20, 10);
+    DomainData::init({}, domain_params, 0);
+
+    size_t size_obstacle_list = 0;
+    auto obstacle_list = new size_t[size_obstacle_list];
+    auto surface_list = new size_t *[number_of_patches];
+    PatchObject size_surface_list;
+    size_t level = 0;
+    Domain domain(obstacle_list, size_obstacle_list, surface_list, size_surface_list, level);
+
+    auto inner_list = domain.get_inner_list();
+    EXPECT_EQ(inner_list[0], 316);
+    EXPECT_EQ(inner_list[domain.get_size_inner_list() - 1], 1839);
+
+    auto boundary_lists = domain.get_boundary_list();
+    auto boundary_sizes = domain.get_size_boundary_list();
+    EXPECT_EQ(boundary_lists[Patch::LEFT][0], 154);
+    EXPECT_EQ(boundary_lists[Patch::BOTTOM][0], 154);
+    EXPECT_EQ(boundary_lists[Patch::FRONT][0], 154);
+    EXPECT_EQ(boundary_lists[Patch::RIGHT][0], 160);
+    EXPECT_EQ(boundary_lists[Patch::TOP][0], 301);
+    EXPECT_EQ(boundary_lists[Patch::BACK][0], 1848);
+
+    EXPECT_EQ(boundary_lists[Patch::LEFT][(*boundary_sizes)[Patch::LEFT] - 1], 1995);
+    EXPECT_EQ(boundary_lists[Patch::BOTTOM][(*boundary_sizes)[Patch::BOTTOM] - 1], 1854);
+    EXPECT_EQ(boundary_lists[Patch::FRONT][(*boundary_sizes)[Patch::FRONT] - 1], 307);
+    EXPECT_EQ(boundary_lists[Patch::RIGHT][(*boundary_sizes)[Patch::RIGHT] - 1], 2001);
+    EXPECT_EQ(boundary_lists[Patch::TOP][(*boundary_sizes)[Patch::TOP] - 1], 2001);
+    EXPECT_EQ(boundary_lists[Patch::BACK][(*boundary_sizes)[Patch::BACK] - 1], 2001);
+
+
+    auto domain_list = domain.get_domain_list();
+    size_t size_domain = domain.get_size_domain_list();
+    EXPECT_EQ(154, domain_list[0]);
+    EXPECT_EQ(2001, domain_list[size_domain - 1]);
+    delete[] surface_list;
+    delete[] obstacle_list;
 }
 
 TEST(DomainTest, indexTestPhysicalDomain) {
@@ -208,7 +262,8 @@ TEST(DomainTest, indexTestPhysicalDomain) {
     Domain domain(obstacle_list, size_obstacle_list, surface_list, size_surface_list, level);
 
     auto inner_list = domain.get_inner_list();
-
+    EXPECT_EQ(inner_list[0], 162);
+    EXPECT_EQ(inner_list[domain.get_size_inner_list() - 1], 1685);
 
     auto boundary_lists = domain.get_boundary_list();
     auto boundary_sizes = domain.get_size_boundary_list();
@@ -230,5 +285,7 @@ TEST(DomainTest, indexTestPhysicalDomain) {
     auto domain_list = domain.get_domain_list();
     size_t size_domain = domain.get_size_domain_list();
     EXPECT_EQ(0, domain_list[0]);
-    EXPECT_EQ(size_domain - 1, domain_list[size_domain - 1]);
+    EXPECT_EQ(1847, domain_list[size_domain - 1]);
+    delete[] surface_list;
+    delete[] obstacle_list;
 }
