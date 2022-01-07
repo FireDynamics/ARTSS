f3737a2c0cdd2aa11a8c44523570871f198149db
My Linh Wuerzburger <linhwuerzburger@gmx.de>
2022-01-07 17:41:12 +0000

    fix

--- src/dataAssimilation/TemperatureSourceChanger.cpp
+++ src/dataAssimilation/TemperatureSourceChanger.cpp
@@ -10,7 +10,7 @@ Settings::data_assimilation::field_changes TemperatureSourceChanger::read_config
     auto file_content = Settings::parse_settings_from_file(filename);
     tinyxml2::XMLDocument doc;
     doc.Parse(file_content.c_str());
-    auto temperature_source = Settings::parse_temperature_source(doc.RootElement(), "temperature");
+    auto temperature_source = Settings::solver::parse_temperature_source(doc.RootElement(), "temperature");
     m_solver_controller.m_solver->replace_heat_source(temperature_source);
     auto changes = Settings::parse_field_changes(doc.RootElement(), "field_changes");
     return changes;
