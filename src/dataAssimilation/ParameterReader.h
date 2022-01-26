b3dfc30b2c18ee27d4a78ed111b6c0ba1e36b0b1
My Linh Würzburger <m.wuerzburger@fz-juelich.de>
2022-01-26 12:25:59 +0100

    formatting

--- src/dataAssimilation/ParameterReader.h
+++ src/dataAssimilation/ParameterReader.h
@@ -7,17 +7,19 @@
 #ifndef ARTSS_DATAASSIMILATION_PARAMETERREADER_H
 #define ARTSS_DATAASSIMILATION_PARAMETERREADER_H
 
+#include <memory>
+
 #include "../interfaces/IParameterReader.h"
 #include "../utility/settings/Settings.h"
 #include "../utility/Utility.h"
 
 class ParameterReader : public IParameterReader {
-public:
+ public:
     ParameterReader();
     ~ParameterReader() = default;
 
     return_parameter_reader read_config(const std::string &filename) override;
-private:
+ private:
     std::shared_ptr<spdlog::logger> m_logger;
 };
 
