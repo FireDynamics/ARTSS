9ef40be8b619f6126fe667490236ec2a2eed3fa2
My Linh Würzburger <linhwuerzburger@gmx.de>
2022-06-03 19:00:49 +0200

    fix: difference has to be always positive (BC)

--- data_assimilation/gradient_based_optimisation.py
+++ data_assimilation/gradient_based_optimisation.py
@@ -88,17 +88,17 @@ def start(fds_data_path: str, fds_input_file_name: str, artss_data_path: str):
         if nabla['x0'] < 0:
             x_max = (domain.domain_param['X2'] - cur['x0']) / -nabla['x0']
         else:
-            x_max = (domain.domain_param['X1'] - cur['x0']) / nabla['x0']
+            x_max = (cur['x0'] - domain.domain_param['X1']) / nabla['x0']
 
         if nabla['y0'] < 0:
             y_max = (domain.domain_param['Y2'] - cur['y0']) / -nabla['y0']
         else:
-            y_max = (domain.domain_param['Y1'] - cur['y0']) / nabla['y0']
+            y_max = (cur['y0'] - domain.domain_param['Y1']) / nabla['y0']
 
         if nabla['z0'] < 0:
             z_max = (domain.domain_param['Z2'] - cur['z0']) / -nabla['z0']
         else:
-            z_max = (domain.domain_param['Z1'] - cur['z0']) / nabla['z0']
+            z_max = (cur['z0'] - domain.domain_param['Z1']) / nabla['z0']
 
         # search direction
         nabla = np.asarray([nabla[x] for x in keys])
