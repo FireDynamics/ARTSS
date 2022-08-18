98c8408057eeb022bef9cbaa143b7ae7c4d09cee
My Linh Wuerzburger <linhwuerzburger@gmx.de>
2022-08-18 10:47:23 -0400

    add: function to replace load_data part from No to Yes, adding file and time

--- tools/change_xml.sh
+++ tools/change_xml.sh
@@ -2,6 +2,8 @@
 HELP="
 replace tags in xml. currently available
 
+--da           \t change XML to start ARTSS at a specific time step. variables:\n\t\t\t1. time step\n\t\t\t2. h5 file name
+h
  -i | --input  \t xml file to be changed
 --logfile      \t specify the new log file name
 --o | --output \t file name of where to write the changes
@@ -15,6 +17,14 @@ do
     -h|--help)
       echo -e "${HELP}"
       exit
+      ;;
+    --da)
+      DA=1
+      DA_TIME=$2
+      DA_FILE=$3
+      shift
+      shift
+      shift
       ;;
     -i | --input)
       INPUT=$2
@@ -45,12 +55,6 @@ then
   exit
 fi
 
-if [ -z $OUTPUT -o -f $OUTPUT ]
-then
-  echo "specify an output file which doesn't exist already"
-  exit
-fi
-
 TMP=.tmp.xml
 cp $INPUT $TMP
 
@@ -66,4 +70,21 @@ if [ ! -z $x0 ]
 then
   echo "change x0 to $x0"
   sed 's/<x0>\s*[0-9]*\.*[0-9]*\s*<\/x0>/<x0> '${x0}' <\/x0>/g' "${TMP}" > "${OUTPUT}"
+  cp $OUTPUT $TMP
+fi
+
+if [ ! -z $DA ]
+then
+  echo "set parameter load_file=Yes; file=${DA_FILE}; time=${DA_TIME}"
+  if grep -xq "\s*<time>\s*[0-9]*\.*[0-9]*\s*</time>\s*" ${TMP}
+  then
+    echo "time is there"
+    sed 's/<time>\s*[0-9]*\.*[0-9]*\s*<\/time>/<time> '${DA_TIME}' <\/time>/g' ${TMP} > ${OUTPUT}
+    cp $OUTPUT $TMP
+    sed 's/load_data=.*>/load_data="Yes" file="'${DA_FILE}'">/g' "${TMP}" > "${OUTPUT}"
+  else
+    echo "time isn't there"
+    sed 's/load_data=.*>/load_data="Yes" file="'${DA_FILE}'">\n    <time> '${DA_TIME}' <\/time>/g' "${TMP}" > "${OUTPUT}"
+  fi 
+  cp $OUTPUT $TMP
 fi
