3829d2ea4d9d235de8f30fad751640df765baede
My Linh Würzburger <m.wuerzburger@fz-juelich.de>
2022-12-01 17:28:06 +0100

    python: added open door to steckler scenario

--- data_assimilation/obstacle_changer.py
+++ data_assimilation/obstacle_changer.py
@@ -95,7 +95,11 @@ def obstacle_wonder(artss_data_path: str):
 
 
 def steckler_door(artss_data_path: str):
-    artss_data_path = os.path.abspath(artss_data_path)
+    """
+    experimental setup to close and open the door
+    :param artss_data_path: path to where ARTSS was started
+    """
+    # artss_data_path = os.path.abspath(artss_data_path)
     client = TCP_client.TCPClient()
     client.connect()
 
@@ -108,48 +112,104 @@ def steckler_door(artss_data_path: str):
     door, neighbours = create_door(obstacles)
 
     time_back = 1
+
+    # close door
     t_sensor = 81 * xml.get_dt() + time_back * xml.get_dt()
-
     wait_artss(t_sensor, artss_data_path)
-
     t_artss, t_revert = get_time_step_artss(t_sensor, artss_data_path, dt=xml.get_dt(),
                                             time_back=time_back * xml.get_dt())
-
-    # da = DAFile()
-    # da.create_config({'u': False, 'v': False, 'w': False, 'p': False, 'T': False, 'C': False})
     domain = Domain(xml.domain, xml.computational_domain, obstacles)
     domain.add_obstacle(door)
-    domain.print_info()
-    print()
     domain.print_debug()
-    da = set_zero(t_artss, t_revert, domain=domain, obstacle_name=door.name, neighbouring_obstacle_patches=neighbours)
-    da.write_obstacle_changes(obstacles + [door], True)
-    # da.remove_obstacle(obstacles, door)
+    da = DAFile()
+    [field_changes, field_file_name] = set_zero(t_artss=t_artss, domain=domain, obstacle_name=door.name,
+                                                neighbouring_obstacle_patches=neighbours,
+                                                artss_data_path=artss_data_path)
+    da.create_field_changes(field_changes, field_file_name=field_file_name)
+    da.write_obstacle_changes(domain.get_obstacles())
     config_file_name = f'change_obstacle_{int(t_sensor * 10)}.xml'
     config_file_path = os.path.join(artss_data_path, config_file_name)
     da.write_xml(config_file_path, pretty_print=True)
-    client.send_message(create_message(t_revert, config_file_path))
+    client.send_message(create_message(t_revert, config_file_name))
+
+    # open door
+    t_sensor = 86 * xml.get_dt() + time_back * xml.get_dt()  # 101
+    wait_artss(t_sensor, artss_data_path)
+    t_artss, t_revert = get_time_step_artss(t_sensor, artss_data_path, dt=xml.get_dt(),
+                                            time_back=time_back * xml.get_dt())
+    door = domain.remove_obstacle(door.name)
+    domain.print_debug()
+    da = DAFile()
+    [field_changes, field_file_name] = set_gradient_x(t_artss=t_artss, domain=domain, obstacle=door,
+                                                      artss_data_path=artss_data_path)
+    da.create_field_changes(field_changes, field_file_name=field_file_name)
+    da.write_obstacle_changes(domain.get_obstacles() + [door])
+    config_file_name = f'change_obstacle_{int(t_sensor * 10)}.xml'
+    config_file_path = os.path.join(artss_data_path, config_file_name)
+    da.write_xml(config_file_path, pretty_print=True)
+    client.send_message(create_message(t_revert, config_file_name))
 
 
-def set_zero(t_artss: float, t_revert: float, obstacle_name: str, domain: Domain, neighbouring_obstacle_patches: Dict[str, List[str]]) -> DAFile:
-    reader = FieldReader(t_artss, path='example')
+def set_gradient_x(t_artss: float, obstacle: Obstacle, domain: Domain, artss_data_path: str) -> [Dict[str, bool], str]:
+    """
+    set gradient in x-direction. interpolate cell values from left neighbour cell to right neighbour cell
+    :param t_artss: time of field to be changed
+    :param obstacle: obstacle which will be removed
+    :param domain: domain of ARTSS
+    :param artss_data_path: path to where ARTSS was executed
+    :return: A dictionary containing the changed fields and the name of changed field file
+    """
+    reader = FieldReader(t_artss, path=artss_data_path)
+    field_T = reader.read_field_data()['T']
+    field_file_name = f'temperature_{t_artss:.5e}'
+
+    for j in range(obstacle.index['y1'], obstacle.index['y2'] + 1):
+        for k in range(obstacle.index['z1'], obstacle.index['z2'] + 1):
+            start_val = field_T[domain.get_index(obstacle.index['x1'] - 1, j, k)]
+            end_val = field_T[domain.get_index(obstacle.index['x2'] + 1, j, k)]
+            delta = (end_val - start_val) / (obstacle.index['x2'] - obstacle.index['x1'])
+            counter = 1
+            for i in range(obstacle.index['x1'], obstacle.index['x2'] + 1):
+                index = domain.get_index(i, j, k)
+                field_T[index] = start_val + delta * counter
+                counter += 1
+
+    FieldReader.write_field_data_keys(file_name=field_file_name,
+                                      data={'T': field_T},
+                                      field_keys=['T'],
+                                      path=artss_data_path)
+    return {'T': True}, field_file_name
+
+
+def set_zero(t_artss: float, obstacle_name: str, domain: Domain,
+             neighbouring_obstacle_patches: Dict[str, List[str]], artss_data_path: str) -> [Dict[str, bool], str]:
+    """
+    set all cells given obstacle to zero in order to see the obstacle in the simulation
+    :param t_artss: time of field to change
+    :param obstacle_name: name of obstacle which cell values should be changed
+    :param domain: domain of ARTSS
+    :param neighbouring_obstacle_patches: specify additional cells which should be changed.
+    E.g. the patch of a neighbour obstacle
+    :param artss_data_path: path to where ARTSS was executed
+    :return:
+    """
+    reader = FieldReader(t_artss, path=artss_data_path)
     field_T = reader.read_field_data()['T']
     domain.set_value_of_obstacle_cells(value=-1, field=field_T, obstacle_name=obstacle_name)
     for o_name in neighbouring_obstacle_patches:
         for patch in neighbouring_obstacle_patches[o_name]:
-            domain.set_value_of_obstacle_patch(value=-1, field=field_T, obstacle_name=o_name, patch=patch)
-    field_file_name = f'temperature_{t_revert:.5e}'
+            domain.set_value_of_obstacle_patch(value=0, field=field_T, obstacle_name=o_name, patch=patch)
+    field_file_name = f'temperature_{t_artss:.5e}'
 
     FieldReader.write_field_data_keys(file_name=field_file_name,
                                       data={'T': field_T},
-                                      field_keys=['T'])
-    da = DAFile()
-    da.create_field_changes({'T': True},
-                            field_file_name=os.path.abspath(field_file_name))
-    return da
+                                      field_keys=['T'],
+                                      path=artss_data_path)
+    return {'T': True}, field_file_name
 
 
-def create_door(obstacles: List[Obstacle], door_identifier: str = None, ground_coordinate: float = 0) -> [Obstacle, Dict[str, List[int]]]:
+def create_door(obstacles: List[Obstacle], door_identifier: str = None, ground_coordinate: float = 0) \
+        -> [Obstacle, Dict[str, List[int]]]:
     """
     door measurements are calculated based on the obstacle names "left from door", "right from door", "above door" +
     door identifier. E.g. door identifier = "Room1" results in looking for obstacle names which include
